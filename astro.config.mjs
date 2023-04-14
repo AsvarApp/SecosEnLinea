159e30eb567b8d99192bdbc68e21bd6d936fec04
carlos segura <nocarlos.x@gmail.com>
2023-04-14 06:23:21 -0500

    1204 new lotery, add integrations astro SEO, Site Map, Optimization img and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), sitemap({
+    filter: page => page !== 'https://stargazers.club/secret-vip-lounge'
+  }), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
